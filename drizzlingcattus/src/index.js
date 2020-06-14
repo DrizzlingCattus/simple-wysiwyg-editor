296325237d4a59bc2a5ae191092304f0217ec1fa
DrizzlingCattus <hit0473@gmail.com>
2020-06-15 03:50:51 +0900

    Add vdom.createElement for virual dom tree
    virtual dom tree 데이터를 실제 dom node로 만들어주는 createElement 모듈
    생성

--- drizzlingcattus/src/index.js
+++ drizzlingcattus/src/index.js
@@ -1,3 +1,83 @@
-const root = document.getElementById('root')
+import editorModel from './models/editor';
 
-root.innerHTML = '<div> hello world </div>'
+const root = document.getElementById('root');
+
+function applyStyleProps(node, styleObject) {
+  const stylePropsList = Object.entries(styleObject);
+  const { style } = node;
+
+  stylePropsList.forEach(([styleName, styleValue]) => {
+    style[styleName] = styleValue;
+  });
+}
+
+function applyProps(node, props) {
+  const propsList = Object.entries(props);
+  propsList.forEach(([propName, propValue]) => {
+    if (propName === 'style') {
+      applyStyleProps(node, propValue);
+    } else {
+      // TODO: check, is vaild property name
+      node.setAttribute(propName, propValue);
+    }
+  });
+}
+
+const VDOM_PREFIX = 'vdom';
+const vdom = {
+  icount: 0,
+
+  createElement(vnode = {}) {
+    const { tagName, props, childs = [] } = vnode;
+
+    const node = document.createElement(tagName);
+    // for node identification
+    node.classList.add(`${VDOM_PREFIX}-${vdom.icount}`);
+    applyProps(node, props);
+    vdom.icount += 1;
+
+    childs.forEach((child) => {
+      if (child) {
+        const childNode = vdom.createElement(child);
+        node.appendChild(childNode);
+      }
+    });
+
+    return node;
+  },
+
+  diff(oldVNode, newVNode) {
+    // TODO
+    return `${oldVNode} ${newVNode}`;
+  },
+
+  patch(vnode, patches) {
+    // TODO
+    return `${vnode} ${patches}`;
+  },
+};
+
+const vtree = editorModel.render();
+console.log('vtree is', vtree);
+
+const editorNode = vdom.createElement(vtree);
+console.log('editorNode is', editorNode);
+
+root.appendChild(editorNode);
+
+const nextVTree = editorModel.renderWithExtra({
+  tagName: 'div',
+  props: {
+    style: {
+      backgroundColor: '#000000', // black
+      width: '20px',
+      height: '20px',
+    },
+  },
+});
+
+const patches = vdom.diff(vtree, nextVTree);
+console.log('patches is', patches);
+
+const nextEditorNode = vdom.patch(editorNode, patches);
+console.log(nextEditorNode);
